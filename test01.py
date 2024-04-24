8f7caf01266a17008ca707d44d150a3c60c2b6c4
Franklin Sales de Oliveira <franklinsales1711@gmail.com>
2024-04-23 21:49:29 -0300

    primeiro commit

---
+++ test01.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Criar um array
+arr = np.array([1, 2, 3, 4, 5])
+
+# Imprimir o array
+print("Array original:", arr)
+
+# Multiplicar todos os elementos do array por 2
+arr = arr * 2
+
+# Imprimir o array modificado
+print("Array modificado:", arr)
